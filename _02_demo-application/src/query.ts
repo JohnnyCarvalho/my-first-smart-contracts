4f8137721ddfc2c73374541db4d453526441f2a4
Johnny Carvalho <Johnny.Carvalho@ibm.com>
2022-09-06 16:36:06 -0300

    Commit
    Finalizando aula tutorial 05 vs code

--- _02_demo-application/src/query.ts
+++ _02_demo-application/src/query.ts
@@ -32,8 +32,9 @@ async function main() {
     const contract = network.getContract('demo-contract');
 
     // Submit the specified transaction.
-    await contract.submitTransaction('createMyAsset', '002', 'Night Watch');
-    console.log('Transaction has been submitted');
+    // Evaluate the specified transaction.
+    const result = await contract.evaluateTransaction('readMyAsset', '002');
+    console.log(`Transaction has been evaluated, result is: ${result.toString()}`);
 
     // Disconnect from the gateway.
     gateway.disconnect();
