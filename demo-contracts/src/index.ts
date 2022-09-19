eaf5957bc62a5ab46098db4435de4a1c5b54c76d
Johnny Carvalho <Johnny.Carvalho@ibm.com>
2022-09-19 11:49:42 -0300

    commit

--- demo-contracts/src/index.ts
+++ demo-contracts/src/index.ts
@@ -2,6 +2,10 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+/**
+ * Delevopel ambient
+ */
+
 import { MyAssetContract } from './my-asset-contract';
 export { MyAssetContract } from './my-asset-contract';
 
